b6eddb69e110be3f6fba5af20e57e25df73a7174
gozooit <gozoo.it@protonmail.com>
2022-05-26 11:43:39 +0200

    calculator can manage simple operations

--- calculator.js
+++ calculator.js
@@ -13,19 +13,62 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    if (!b === 0) {
+    if (b) {
         return a / b;
     } 
 };
 
 function operate(operator, a, b) {
+    if (typeof(a) !== "number") a = parseInt(a);
+    if (typeof(b) !== "number") b = parseInt(b);
     if (operator === "add") {
-        add(a, b);
+        res = add(a, b);
     } else if (operator === "subtract") {
-        subtract(a, b);
+        res = subtract(a, b);
     } else if (operator === "multiply") {
-        multiply(a, b);
+        res = multiply(a, b);
     } else if (operator === "divide") {
-        divide(a, b);
+        res = divide(a, b);
     }
+    return res.toString();
 };
+
+function updateDisplay(value) {
+    const displayContent = document.querySelector('#display p');
+    displayContent.textContent = value;
+}
+
+function simpleOperation(e) {
+    // console.log(e);
+    // console.log(e.target.id);
+    // console.log(e.target.id.match("digit*"))
+    if (e.target.id.match("digit*")) {
+        displayValue += e.target.textContent;
+        updateDisplay(displayValue);
+    } else if (operators.includes(e.target.id)) {
+        storedValue = displayValue;
+        pendingOperation = e.target.id;
+        displayValue = "";
+        updateDisplay(displayValue);
+    } else if ((e.target.id === "equal") && pendingOperation) {
+        displayValue = operate(pendingOperation, storedValue, displayValue);
+        updateDisplay(displayValue);
+        pendingOperation = null;
+    } else if (e.target.id === "clear") {
+        storedValue = "";
+        displayValue = "";
+        pendingOperation = null;
+        updateDisplay(displayValue);
+    }
+    // console.log(`storedValue = ${storedValue}`);
+    // console.log(`displayValue = ${displayValue}`);
+    // console.log(`pendingOpera = ${pendingOperation}`);
+}
+
+let displayValue = "";
+let storedValue;
+let pendingOperation = null;
+const operators = ["add", "subtract", "multiply", "divide"];
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('click', simpleOperation));
