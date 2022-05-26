4d5bbdbd04a83e484f4076e739eb59d4f10337b0
gozooit <gozoo.it@protonmail.com>
2022-05-26 19:03:35 +0200

    calculator working, begin to add new features

--- calculator.js
+++ calculator.js
@@ -38,66 +38,105 @@ function updateDisplay(value) {
     displayContent.textContent = value;
 }
 
-function simpleOperation(e) {
-    // console.log(e);
-    // console.log(e.target.id);
-    // console.log(e.target.id.match("digit*"))
-    if (e.target.id.match("digit*")) {
-        displayValue += e.target.textContent;
-        updateDisplay(displayValue);
-    } else if (operators.includes(e.target.id)) {
-        storedValue = displayValue;
-        pendingOperation = e.target.id;
-        displayValue = "";
-        updateDisplay(displayValue);
-    } else if ((e.target.id === "equal") && pendingOperation) {
-        displayValue = operate(pendingOperation, storedValue, displayValue);
-        updateDisplay(displayValue);
-        pendingOperation = null;
-    } else if (e.target.id === "clear") {
-        storedValue = "";
-        displayValue = "";
-        pendingOperation = null;
-        updateDisplay(displayValue);
+function plusMinus(value) {
+    arr = value.split("");
+    if (arr[0] === "-") {
+        arr.shift();
+    } else {
+        arr.unshift("-");
     }
-    // console.log(`storedValue = ${storedValue}`);
-    // console.log(`displayValue = ${displayValue}`);
-    // console.log(`pendingOpera = ${pendingOperation}`);
 }
 
-function chainOperation(e) {
+function calculator(e) {
     if (e.target.id.match("digit*")) {
         displayValue += e.target.textContent;
         updateDisplay(displayValue);
-    } else if (operators.includes(e.target.id)) {
-        chainValue.push(displayValue);
-        chainValue.push(e.target.id);
-        displayValue = "";
+    } else if (e.target.id === "plusminus") {
+        displayValue = plusMinus(displayValue);
         updateDisplay(displayValue);
-    } else if ((e.target.id === "equal")) {
-        chainValue.push(displayValue);
-        chainValue.push(e.target.id);
-        for (i = 0; i < chainValue.length; i++) {
-            if (operators.includes(chainValue[i])) {
-                chainValue[i + 1] = operate(chainValue[i], chainValue[i - 1], chainValue[i + 1]);
-            } else if (chainValue[i] === "equal") {
-                displayValue = chainValue[i - 1];
-                updateDisplay(displayValue);
-            }
+    } else if (operators.includes(e.target.id)) {
+        if (lastValue === "0") {
+            lastValue = displayValue;
+        } else {
+            lastValue = operate(lastOperator, lastValue, displayValue);
         }
-    } else if (e.target.id === "clear") {
-        chainValue = [];
+        lastOperator = e.target.id;
+        updateDisplay(lastValue);
         displayValue = "";
+    } else if ((e.target.id === "equal")) {
+        console.log(`lastvalue: ${lastValue} `, `lastop: ${lastOperator}`, !lastValue === "0");
+        if (!lastOperator || !displayValue) {
+            displayValue = lastValue;
+        } else {
+            displayValue = operate(lastOperator, lastValue, displayValue);
+        }
         updateDisplay(displayValue);
+        lastOperator = null;
+        console.log(`lastvalue: ${lastValue} `, `lastop: ${lastOperator}`, !lastValue === "0");
+    } else if (e.target.id === "clear") {
+        displayValue = "";
+        lastValue = "0";
+        lastOperator = null;
+        updateDisplay(lastValue);
     }
-    console.log(chainValue);
 }
 
-let displayValue = "";
-let storedValue;
-let chainValue = [];
-let pendingOperation = null;
+// Initialize var and set display to 0
 const operators = ["add", "subtract", "multiply", "divide"];
+let displayValue = "";
+let lastValue = "0";
+let lastOperator = null;
+updateDisplay(lastValue);
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('click', chainOperation));
+keys.forEach(key => key.addEventListener('click', calculator));
+
+
+// function chainOperation(e) {
+//     if (e.target.id.match("digit*")) {
+//         displayValue += e.target.textContent;
+//         updateDisplay(displayValue);
+//     } else if (operators.includes(e.target.id)) {
+//         chainValue.push(displayValue);
+//         chainValue.push(e.target.id);
+//         displayValue = "";
+//         updateDisplay(displayValue);
+//     } else if ((e.target.id === "equal")) {
+//         chainValue.push(displayValue);
+//         chainValue.push(e.target.id);
+//         for (i = 0; i < chainValue.length; i++) {
+//             if (operators.includes(chainValue[i])) {
+//                 chainValue[i + 1] = operate(chainValue[i], chainValue[i - 1], chainValue[i + 1]);
+//             } else if (chainValue[i] === "equal") {
+//                 displayValue = chainValue[i - 1];
+//                 updateDisplay(displayValue);
+//             }
+//         }
+//     } else if (e.target.id === "clear") {
+//         chainValue = [];
+//         displayValue = "";
+//         updateDisplay(displayValue);
+//     }
+//     console.log(chainValue);
+// }
+
+// function simpleOperation(e) {
+//     if (e.target.id.match("digit*")) {
+//         displayValue += e.target.textContent;
+//         updateDisplay(displayValue);
+//     } else if (operators.includes(e.target.id)) {
+//         storedValue = displayValue;
+//         pendingOperation = e.target.id;
+//         displayValue = "";
+//         updateDisplay(displayValue);
+//     } else if ((e.target.id === "equal") && pendingOperation) {
+//         displayValue = operate(pendingOperation, storedValue, displayValue);
+//         updateDisplay(displayValue);
+//         pendingOperation = null;
+//     } else if (e.target.id === "clear") {
+//         storedValue = "";
+//         displayValue = "";
+//         pendingOperation = null;
+//         updateDisplay(displayValue);
+//     }
+// }
